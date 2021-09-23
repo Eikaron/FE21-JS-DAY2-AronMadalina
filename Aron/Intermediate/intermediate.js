dbbe8f919e519c37672e9fd517b29b514cfa931f
Aron Faulhaber <aron.faulhaber@gmail.com>
2021-09-23 11:19:27 +0200

    int1 finished

--- Aron/Intermediate/intermediate.js
+++ Aron/Intermediate/intermediate.js
@@ -1,12 +1,14 @@
-let string = "i am a web developer";
+//Why can we not pass argument from outside?
 
-let newString = function(string){
+let newString = function(str="i am a web developer"){
 
-    let firstletter = string.charAt(0);
-    let firstletter2 = firstletter.toUpperCase()
-    
-    let string2 = string.slice(1)
-    console.log(string2)
+    let firstletter = str.charAt(0);
+    let firstletterCap = firstletter.toUpperCase();
+
+    let strSliced = str.slice(1);
+
+    let newString = firstletterCap+strSliced
+    console.log(newString);
 }
 
-newString()
+newString();
